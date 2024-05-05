4dfa664f1523a74a2ab5465d5ef0e662b8da58bb
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2024-05-05 12:06:49 +0530

    changes

--- src/layouts/BidPlacePage/BidPlaceAndReviewBox.tsx
+++ src/layouts/BidPlacePage/BidPlaceAndReviewBox.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import BookModel from "../../models/AuctionModel";
 import AuctionModel from "../../models/AuctionModel";
 import { useOktaAuth } from "@okta/okta-react";
+import { useState } from "react";
 
 export const BidPlaceAndReviewBox: React.FC<{
   auction: AuctionModel | undefined;
@@ -9,6 +10,31 @@ export const BidPlaceAndReviewBox: React.FC<{
   
 }> = (props) => {
   const { authState } = useOktaAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [bidAmount, setBidAmount] = useState('');
+  const [bidComment, setBidComment] = useState('');
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/Bid/placeBid', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amount: bidAmount, comment: bidComment , auction_id: props.auction?.id , user_name:authState?.idToken?.claims.preferred_username , placed_at: new Date()}),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Handle success (e.g., show a success message, close the modal, etc.)
+      handleCloseModal();
+    } catch (error) {
+      console.error('There was a problem with your fetch operation:', error);
+      // Handle error (e.g., show an error message)
+    }
+  };
   return (
     <div
       className={
@@ -16,34 +42,16 @@ export const BidPlaceAndReviewBox: React.FC<{
       }
     >
         <div className='card-body container'>
-            <div className="mt-3">
-              <p>
-                <b>0/5</b>
-                books checked out
-              </p>
-            </div>
-            <hr/>
-           {props.auction && props.auction.startingPrice && props.auction.startingPrice > 0 ?
-           <h4 className='text-success'>
-            Available
-           </h4>
-           :
-           <h4 className='text-danger'>
-                Wait List
-           </h4>
-           }
+            <h2 className='card-title'>{props.auction?.name}</h2>
            <div className='row'>
               <p className='col-6 lead'>
-             <b>
-                {props.auction?.closingTime}
-             </b>
-             copies
-              </p>
+              <p>Closing Time</p>
+              <p>{props.auction?.closingTime}</p>
+             </p>
               <p className='col-6 lead'>
-             <b>
-                {props.auction?.startingPrice}
-             </b>
-             available
+                <p>Starting Price</p>
+                <p>{props.auction?.startingPrice}</p>
+            
               </p>
            </div>
         </div>
@@ -52,22 +60,81 @@ export const BidPlaceAndReviewBox: React.FC<{
             type="button"
             className="btn main-color btn-lg text-white"
             to={`/checkout/${props.auction?.id}`} 
+            onClick={handleOpenModal}
           >
             Place a Bid
           </Link>
         ) : (
           <Link className="btn main-color btn-lg text-white" to="/login">
-            Sign in
+            Sign Up
           </Link>
         )
           }
         <hr/>
-        <p className='mt-3'>
-          This number can change until placing order has been complete.
-        </p>
-        <p>
-            Sign in to be able to leave a review.
-        </p>
+        {isModalOpen && (
+          <div style={{
+            position: 'fixed',
+            zIndex: 1,
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+            overflow: 'auto',
+            backgroundColor: 'rgba(0,0,0,0.4)',
+            borderRadius: '20px',
+
+          }}>
+            <div style={{
+              backgroundColor: '#fefefe',
+              margin: '15% auto',
+              padding: '20px',
+              border: '1px solid #888',
+              width: '500px',
+              borderRadius: '20px',
+            }}>
+              <span style={{float: 'right', fontSize: '28px', cursor: 'pointer'}} onClick={handleCloseModal}>&times;</span>
+              <h3 >Place a Bid</h3>
+              <label className=" my-1 text-secondary"> Fill in the details below to place a bid!</label>
+              <form>
+                <div>
+                  <label className=" form-label" htmlFor="bidAmount">Bid Amount</label>
+                  <input
+                    className="form-control"
+                    type="number"
+                    id="bidAmount"
+                    value={bidAmount}
+                    onChange={(e) => setBidAmount(e.target.value)}
+                  />
+                </div>
+                <div className=" mt-3">
+                  <label className=" form-label" htmlFor="bidComment">Comment</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="bidComment"
+                    value={bidComment}
+                    onChange={(e) => setBidComment(e.target.value)}
+                  />
+                </div>
+                <div className=" pt-2 mt-2" >
+                <button
+                  className="btn btn-success btn-sm mx-2"
+                  style={{width:"70px"}}
+                  type="button"
+                  onClick={(e) => {
+                    e.preventDefault(); // Prevent default form submission behavior
+                    handleSubmit(); // Call your custom submit handler
+                    handleCloseModal(); // Close the modal after submission
+                  }}>
+                  OK
+                </button>
+
+                  <button className=" btn btn-outline-danger btn-sm mx-1" style={{width:"70px"}} type="button" onClick={handleCloseModal}>Cancel</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
---
+++ src/layouts/BidPlacePage/ViewAllBids.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+export default function ViewAllBids(props) {
+    const [bids, setBids] = useState([]);
+    let auction_id = props.value; 
+
+
+    useEffect(() => {
+        fetchBids();
+    }, []);
+
+    const fetchBids = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/Bid/getBidOnItem/${auction_id}`);
+            setBids(response.data.content);
+            console.log(auction_id);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+  return (
+    <div className=' container shadow '>
+        <div className='h3 m-3'>
+            All Bids
+        </div>
+        <div>
+            <table className=' table'>
+                <thead>
+                    <tr>
+                        
+                        <th>Placed By</th>
+                        <th>Placed At</th>
+                        <th>Comment</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        bids.map((bid) => (
+                            <tr key={bid.id}>
+                                <td className=' text-primary'>{bid.user_name}</td>
+                                <td>{bid.placed_at}</td>
+                                <td>{bid.comment}</td>
+                                <td className=' text-success'>{bid.amount}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+  )
+}
