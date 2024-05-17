6a0fca7103ae6375a5d9a8b56035b1c3c7e4c5ef
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-05-17 11:34:43 +0530

    Update Footer.tsx

--- src/layouts/NavbarAndFooter/Footer.tsx
+++ src/layouts/NavbarAndFooter/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div className='main-color'>
             <footer className='container d-flex flex-wrap 
                 justify-content-between align-items-center py-5 main-color'>
-                <p className='col-md-4 mb-0 text-white'>© Example Library App, Inc</p>
+                <p className='col-md-4 mb-0 text-white'>© Bidding App, Inc</p>
                 <ul className='nav navbar-dark col-md-4 justify-content-end'>
                     <li className='nav-item'>
                         <Link className='nav-link px-2 text-white' to={"/home"}>
@@ -14,7 +14,7 @@ export const Footer = () => {
                     </li>
                     <li className='nav-item'>
                         <Link  className='nav-link px-2 text-white' to={"/search"}>
-                            Search Books
+                            Search Products
                         </Link>
                     </li>
                 </ul>
